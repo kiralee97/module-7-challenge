6eaeb885e8c4530f2bc1a4f4c458a7ad9f800086
kiralee97 <kiralee97@gmail.com>
2025-01-30 11:54:03 -0600

    npm i inquirer

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 // TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  inquirer.prompt ([
+    {
+
+    },
+  ])
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
